5de90cbc98802e1b24d80657fe832483f28c9739
Marius <marius.guerard@gmail.com>
2020-06-01 21:39:05 -0700

    Added helper_function.py and viz.py

---
+++ helper_function.py
@@ -0,0 +1,121 @@
+import time
+
+import numpy as np
+
+
+class ClosestMean():
+
+    def __init__(self, geometry='euclid'):
+        self.mean_dic = dict()
+        self.result_dic = dict()
+        if geometry == 'euclid':
+            self.mean_func = lambda M_vec: np.mean(M_vec, axis=0)
+            self.norm_func = lambda M1, M2: ((M1 - M2)**2).sum()
+        self.geometry = geometry
+
+    def fit(self, cov_train, y_train):
+        self.labels = list(set(y_train))
+        for label in self.labels:
+            idx_label = np.where(y_train==label)[0]
+            #cov_mean_label = np.mean(cov_train[idx_label], axis=0)
+            cov_mean_label = self.mean_func(cov_train[idx_label])
+            self.mean_dic[label] = cov_mean_label
+            
+    def predict(self, cov_test):
+        yest_test = []
+        for cov_sample in cov_test:
+            dist_to_means = [self.norm_func(cov_sample, self.mean_dic[label]) for label in self.labels]
+            yest_test.append(np.argmin(dist_to_means))
+        return yest_test
+
+    # To make it scikit-learn compatible.
+
+    def get_params(self, deep=True):
+        return {'geometry': self.geometry}
+    
+    def score(self, cov_test, y_test):
+        yest_test = self.predict(cov_test)
+        return accuracy(y_test, yest_test)
+    
+    
+def vectorize_cov(cov_mat):
+    """Transform a covariance matrix into an n(n+1)/2 elements vector.
+    """
+    lcov = len(cov_mat)
+    covec = []
+    for i in range(lcov):
+        for j in range(i + 1):
+            covec.append(cov_mat[i][j])
+    return covec
+    
+def accuracy(yest, y):
+    """Accuracy computed between estimated class and true class vectors.
+    """
+    yest = np.array(yest)
+    y = np.array(y)
+    return (yest == y).sum() / len(y)
+
+def evaluate_on_kfold(model, X, y, kfold):
+    """From a StratifiedKFold object, fit and test the model and return the accuracy results.
+    Allow to compare different model on the same split and works with all models that have a 'fit' 
+    and 'predict' method.
+    """
+    acc = []
+    for train_indices, test_indices in kfold.split(X, y):
+        X_train = X[train_indices]
+        y_train = y[train_indices]
+        X_test = X[test_indices]
+        y_test = y[test_indices]
+        model.fit(X_train, y_train)
+        yest_test = model.predict(X_test)
+        acc.append(accuracy(yest_test, y_test))
+    return acc
+    
+def record_results_kfold(dic_data, dic_result, model, kfold, key_result, key_data='var'):
+    dic_result[key_result] = {}
+    dic_result[key_result]['acc'] = {}
+    dic_result[key_result]['t'] = {}
+    dic_result[key_result]['model'] = {}
+    dic_result[key_result]['acc_avg'] = {}
+    for key, data in dic_data.items():
+        a = time.time()
+        X = data[key_data]
+        y = data['labels']
+        acc = evaluate_on_kfold(model, X, y, kfold)
+        time_eval = (time.time() - a) / kfold.n_splits
+        dic_result[key_result]['acc'].setdefault(key, []).append(acc)
+        dic_result[key_result]['t'].setdefault(key, []).append(time_eval)
+        dic_result[key_result]['model'].setdefault(key, []).append(model)
+        dic_result[key_result]['acc_avg'].setdefault(key, []).append(np.mean(acc))
+        print(key, acc, np.mean(acc))
+    dic_result[key_result]['acc_all'] = np.array([x for _, x in dic_result[key_result]['acc'].items()]).flatten()
+    dic_result[key_result]['t_all'] = np.array([x for _, x in dic_result[key_result]['t'].items()]).flatten()
+    return dic_result
+
+def train_test_intersession(dic_data, dic_result, model, key_result, data_type='cov'):
+    dic_result[key_result] = {'acc': {}, 't': {}, 'model': {}}
+    score_list = []
+    for train_key, data in dic_data.items():
+        if 's1' in train_key:
+            test_key = train_key[:-2] + '2*'
+        elif 's2' in train_key:
+            test_key = train_key[:-2] + '1*'
+        data_train = dic_data[train_key]
+        data_test = dic_data[test_key]
+        X_train = data_train[data_type]
+        X_test = data_test[data_type]
+        y_train = data_train['labels']
+        y_test = data_test['labels']          
+        a = time.time()
+        model.fit(X_train, y_train)
+        yest_test = model.predict(X_test)
+        score = accuracy(yest_test, y_test)
+        time_eval = time.time() - a
+        score_list.append(score)
+        key_score = '{}-{}'.format(train_key, test_key)
+        dic_result[key_result]['acc'].setdefault(key_score, []).append(score)
+        dic_result[key_result]['t'].setdefault(key_score, []).append(time_eval)
+        dic_result[key_result]['model'].setdefault(key_score, []).append(model)
+        print(train_key, test_key, score)
+    print('avg: {}'.format(np.mean(score_list)))
+    return dic_result
---
+++ viz.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def bar_plot_scatter_comp(list_vec, x_labels, title, legend, save_name=None, stats_list=None, xlabel='Model', ylabel='Acc'):
+    """Bar plot of each elements of list_vec. 
+    (For example if list_vec = [vec_A, vec_B] it will plot 2 bars 
+    for each time point along error bar and superimposed with scatterplot).
+    Args:
+        - list_vec ([np.array]): the observed values of dimension N_samples, N_time.
+        - x_labels ([int]): list of integers specifying the x values.
+        - title (str): Title of the plot.
+        - legend ([str]): one string per vector of list_vec.
+        - save_name (str): Save the figure if a path is provided.
+        - stats_list ([float]): One p-value for each time point corresponding 
+        to the prob. of H0: "<list_vec[0]> = <list_vec[1]>".
+    """
+    ### Function to return a vector (of length 'size') made of random number between -1 and 1.
+    rand_m1 = lambda size: np.random.random(size) * 2 - 1
+
+    ### Plots parameters.
+    N_serie = len(list_vec)
+    label_loc = np.arange(len(x_labels))  # the label locations
+    w = 0.5  # the width of the bars
+    colors = ['b', 'r']
+    center_bar = [w * (i - 0.5) for i in range(N_serie)]
+
+    ### Plots values.
+    avg_conc = [np.nanmean(conc, axis=0) for conc in list_vec]
+    std_conc = [np.nanstd(conc, axis=0) for conc in list_vec]
+        
+    ### Plotting the data.
+    # if figure is None:
+    #     fig = plt.figure(figsize=(15,10))
+    # ax = plt.gca()
+    fig, ax = plt.subplots(figsize=(20, 15))
+    
+    for i, conc in enumerate(list_vec):
+        label_i = legend[i] + ' (n = {})'.format(len(conc))
+        rects = ax.bar(label_loc + center_bar[i], avg_conc[i], w, label=label_i,
+                       yerr=std_conc[i], color=colors[i], alpha=0.6)
+        # Add the single point with a scatter distributed randomly across whole width of bar.
+        for j, x in enumerate(label_loc):
+            ax.scatter((x + center_bar[i]) + rand_m1(conc[:, j].size) * w/4,
+                       conc[:, j], color=colors[i], edgecolor='k')
+
+    ### Plot description.
+    # Stats if provided.
+    if stats_list is not None:
+        for i, loc in enumerate(label_loc):
+            # plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
+            if stats_list[i] < 0.05:
+                y = max(std_conc[0][i] + avg_conc[0][i], std_conc[1][i] + avg_conc[1][i]) + 4
+                plt.text(loc, y, "*", ha="center", va="bottom", fontsize=30)     
+    
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.set_xticks(label_loc)
+    ax.set_xticklabels(x_labels)
+    ax.legend()
+    plt.legend()       
+    ### Saving the figure with a timestamp as a name.
+    if save_name is not None:
+        plt.savefig(save_name)
